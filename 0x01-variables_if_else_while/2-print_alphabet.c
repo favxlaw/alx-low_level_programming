b214d0a595ae904b97545da4882aaf0eb4059b85
favxlaw <nmesomafavour2004@gmail.com>
2022-09-11 20:09:01 -0700

    fixed-2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Got a random number and check its las digit, compare it with a
+ * Return: o
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
