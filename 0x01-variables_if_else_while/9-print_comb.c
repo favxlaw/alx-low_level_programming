fbf0f93e1ca20aadf05095b80fbd6f3f7b99f52a
favxlaw <nmesomafavour2004@gmail.com>
2022-09-11 20:23:20 -0700

    fixed-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- Enter main
+ * Description: print out all single digits
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
