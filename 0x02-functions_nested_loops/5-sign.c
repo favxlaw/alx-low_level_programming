e8e602fb742749b5cf89ca5d45f02625bcafb464
favxlaw <nmesomafavour2004@gmail.com>
2022-09-12 04:55:24 -0700

    fixed-5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign- entry point
+ * @n : prints the sign of a number.
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	int num;
+
+	if (n > 0)
+	{
+		num = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		num = 0;
+		_putchar('0');
+	}
+	else
+	{
+		num = -1;
+		_putchar('-');
+	}
+	return (num);
+}
