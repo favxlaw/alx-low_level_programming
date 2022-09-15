01d9a977ef4515202d763883a2ab28281b7de6d2
favxlaw <nmesomafavour2004@gmail.com>
2022-09-14 22:19:09 -0700

    fixed-o

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test function that prints positive or negative numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
