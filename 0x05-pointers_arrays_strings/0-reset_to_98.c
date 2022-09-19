11ee42bb29751ab14fe392c5d3831ff8e7d8721a
favxlaw <nmesomafavour2004@gmail.com>
2022-09-19 15:25:15 -0700

    fixed-0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that updates the reference of a pointer
+ * @n: pointer to n to be updated
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
