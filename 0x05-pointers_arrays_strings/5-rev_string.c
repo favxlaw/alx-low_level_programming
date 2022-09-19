0cb58889cd876800fb0d018201414ace02ad4055
favxlaw <nmesomafavour2004@gmail.com>
2022-09-19 16:13:26 -0700

    fixed-5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,24 @@
 #include "main.h"
+/**
+ * rev_string - prints reversed string
+ * @s: pointer
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char str[500];
+
+	while (*(s + i))
+	{
+		*(str + i) = *(s + i);
+		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		*(s + i) = *(str + j);
+		j++;
+		i--;
+	}
+}
