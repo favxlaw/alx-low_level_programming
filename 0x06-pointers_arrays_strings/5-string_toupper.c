82574c341c53a2d79de4b64b800be262b5a2f933
favxlaw <nmesomafavour2004@gmail.com>
2022-09-22 17:25:44 -0700

    fixed-5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,18 @@
-if (s[i] >= 97 && s[i] <= 122)
+#include "main.h"
+/**
+ * string_toupper- function that changes all lowercase letters of
+ * a string to uppercase.
+ * @s: input string
+ * Return: Always 0
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
 			s[i] = s[i] - 32;
 	}
-	return (0);
 	return (s);
 }
