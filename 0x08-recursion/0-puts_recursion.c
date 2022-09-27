177607b3dafa05bdae59ce2a138aa8cd10f9327d
favxlaw <nmesomafavour2004@gmail.com>
2022-09-27 03:49:13 -0700

    fixed-0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion- Function that prints a string
+ * @s: string
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
