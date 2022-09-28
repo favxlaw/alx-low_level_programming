dc4ca43cca5961202e578adca6038f60b6f7d6e7
favxlaw <nmesomafavour2004@gmail.com>
2022-09-27 22:31:10 -0700

    fixed-0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
