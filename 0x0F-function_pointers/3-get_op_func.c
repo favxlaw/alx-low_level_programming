6feda374326011c6f6f4552ff277ca359e59a744
favxlaw <nmesomafavour2004@gmail.com>
2022-10-14 00:37:48 -0700

    fixed-3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,69 +1,32 @@
-/**
- * op_add - adds @a and @b
- *
- * @a: input 1
- * @b: input 2
- *
- * Return: sum of @a and @b
-*/
-
-int op_add(int a, int b)
-{
-	return (a + b);
-}
-
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * op_sub - gets the difference of @a and @b
- *
- * @a: input 1
- * @b: input 2
- *
- * Return: difference of @a and @b
-*/
-int op_sub(int a, int b)
+ * get_op_func - select the correct operation function asked by user
+ * @s: operator argument
+ * Return: function pointer corresponding to operator given
+ */
+int (*get_op_func(char *s))(int, int)
 {
-	return (a - b);
-}
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
+	int i;
 
-/**
- * op_mul - multiplies @a and @b
- *
- * @a: input 1
- * @b: input 2
- *
- * Return: product of @a and @b
-*/
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
+	i = 0;
+	while (i < 5)
+	{
+		if (ops[i].op[0] == s[0])
+			return (ops[i].f);
+		i++;
+	}
 
-
-/**
- * op_div - divides @a by @b
- *
- * @a: input 1
- * @b: input 2
- *
- * Return: results of the division @a and @b
-*/
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-
-
-/**
- * op_mod - gets the reminder of the division between @a and @b
- *
- * @a: input 1
- * @b: input 2
- *
- * Return: the remainder
-*/
-int op_mod(int a, int b)
-{
-	return (a % b);
+	return (NULL);
 }
